d43f3f4e0de708e827259c9721c94f7d245521a6
AdrianBlendea <114909191+AdrianBlendea@users.noreply.github.com>
2024-05-25 16:20:43 +0300

    Initial commit

---
+++ src/main/java/com/example/programingappapi/ProgramingappApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.programingappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramingappApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramingappApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/programingappapi/ProgramingappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programingappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramingappApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
