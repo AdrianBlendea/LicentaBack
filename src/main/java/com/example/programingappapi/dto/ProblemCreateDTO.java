5ff120b4b2bbdbfc81ed8aec52191eb00523dca6
AdrianBlendea <114909191+AdrianBlendea@users.noreply.github.com>
2024-06-29 13:35:50 +0300

    small change

--- src/main/java/com/example/programingappapi/dto/ProblemCreateDTO.java
+++ src/main/java/com/example/programingappapi/dto/ProblemCreateDTO.java
@@ -6,6 +6,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 
 import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,16 +18,16 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class ProblemCreateDTO {
-
+    @NotNull
     private String name;
 
-
+    @NotNull
     private String requirement;
-
+    @NotNull
     Long typeId;
-
+    @NotNull
     private Long procentToPass;
 
-
+    @NotNull
     List<TestCaseCreateDTO> testList;
 }
